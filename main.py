94d3138bcd0eea2288be2df3faab9f387e1b48c2
Brandon <brandonrafael80@gmail.com>
2024-12-09 11:38:03 +0700

    feat: adding docker file and pyenv

--- main.py
+++ main.py
@@ -8,10 +8,25 @@ from model import FasterRCNNResNet50
 from PIL import Image
 
 import io
+import requests
+
+# Download the checkpoint
+MODEL_PATH = "checkpoint.pth"
+HF_URL = (
+    "https://huggingface.co/brandonRafael/outfit-ai/resolve/main/checkpoint_epoch_4.pth"
+)
+
+
+def download_pth():
+    response = requests.get(HF_URL)
+
+    with open(MODEL_PATH, "wb") as f:
+        f.write(response.content)
 
 
 # Initialize the model and load the checkpoint
-checkpoint = torch.load("checkpoint.pth")
+download_pth()
+checkpoint = torch.load(MODEL_PATH)
 model = FasterRCNNResNet50()
 model.load_state_dict(checkpoint["model_state_dict"])
 model.eval()
@@ -21,11 +36,14 @@ model.eval()
 class ImageData(BaseModel):
     image_bytes: bytes
 
+
 def preprocess_image(image_bytes):
-    transform = transforms.Compose([
-        transforms.Resize((800, 800)),
-        transforms.ToTensor(),
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize((800, 800)),
+            transforms.ToTensor(),
+        ]
+    )
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
     return transform(image)
 
@@ -33,9 +51,10 @@ def preprocess_image(image_bytes):
 # Initialize the FastAPI app
 app = FastAPI()
 
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    image_bytes = await file.read() 
+    image_bytes = await file.read()
     image_tensor = preprocess_image(image_bytes)
     image_input = list(image_tensor.unsqueeze(0))
 
@@ -43,20 +62,23 @@ async def predict(file: UploadFile = File(...)):
         pred = model(image_input)[0]
 
     score_threshold = 0.5
-    scores = pred['scores']
+    scores = pred["scores"]
     mask = scores >= score_threshold
 
-    filtered_boxes = pred['boxes'][mask]
-    filtered_labels = pred['labels'][mask]
-    filtered_scores = pred['scores'][mask]
+    filtered_boxes = pred["boxes"][mask]
+    filtered_labels = pred["labels"][mask]
+    filtered_scores = pred["scores"][mask]
 
     return {
         "boxes": filtered_boxes.tolist(),
         "labels": filtered_labels.tolist(),
-        "scores": filtered_scores.tolist()
+        "scores": filtered_scores.tolist(),
     }
+
 
 # Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
+

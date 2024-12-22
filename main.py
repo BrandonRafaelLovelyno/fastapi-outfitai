eeee5fab3d60b578bddb202761e169436c284e9d
Brandon Rafael Lovelyno <brandonrafael80@gmail.com>
2024-12-23 03:16:31 +0700

    fix: cors middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importing the necessary libraries
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 import torch
@@ -50,6 +51,14 @@ def preprocess_image(image_bytes):
 
 # Initialize the FastAPI app
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from all origins
+    allow_credentials=True,  # Allow cookies and other credentials
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 async def check_availability():
